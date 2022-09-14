5c27d307d64be8b590430fa600b37a6b7e8d44fd
Fabian <fabianlopez1988@gmail.com>
2022-09-14 17:37:40 -0300

    wed

---
+++ src/components/Header/Header.js
@@ -0,0 +1,18 @@
+import React from "react";
+import trailer from "../../assets/videoUC.mp4";
+import "./Header.css";
+
+const Header = () => {
+  return (
+    <div className="containerVideo">
+      <div className="overlay"></div>
+      <video src={trailer} autoPlay loop muted></video>
+      <div className="headerContainer parpadea text">
+        <h1 className="titulo1">NUESTRO CLUB </h1>
+        <h1 className="titulo2">NUESTRO HOGAR</h1>
+      </div>
+    </div>
+  );
+};
+
+export default Header;
