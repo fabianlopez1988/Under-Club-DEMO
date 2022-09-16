9b87d158e2c50c822d265e4fc2b291cd7642720a
MatiasNasif <matiasezequielnasif@gmail.com>
2022-09-15 21:14:33 -0300

    login admin

--- src/App.js
+++ src/App.js
@@ -6,6 +6,7 @@ import NavBar from "./components/NavBar/NavBar";
 import History from "./components/History/History";
 import Artists from "./components/Artists/Artists";
 import Home from "./components/Home/Home";
+import Login from "./components/LoginAdmin/Login";
 
 function App() {
   return (
@@ -16,6 +17,8 @@ function App() {
           <Route path="/" element={<Home />} />
           <Route path="/artists" element={<Artists />} />
           <Route path="/history" element={<History />} />
+
+          <Route path="/admin" element={<Login />} />
         </Routes>
       </BrowserRouter>
     </div>
---
+++ src/components/LoginAdmin/Login.js
@@ -0,0 +1,64 @@
+import "./Login.css";
+import imgLogin from "../../assets/imgLogin.jpg";
+import Form from "react-bootstrap/Form";
+// import useInput from "../utils/useInput.js";
+// import { useDispatch } from "react-redux";
+// import { userLogin } from "../store/user";
+// import { useNavigate } from "react-router";
+// import { useEffect } from "react";
+// import { Link } from "react-router-dom";
+
+const Login = () => {
+  // const email = useInput();
+  // const password = useInput();
+
+  // const dispatch = useDispatch();
+  // const navigate = useNavigate();
+
+  //   const handleSubmit = (e) => {
+  //     e.preventDefault();
+  //     dispatch(
+  //       userLogin({
+  //         email: email.value,
+  //         password: password.value,
+  //       })
+  //     )
+  //       .then(() => {
+  //         if (localStorage.getItem("user").admin === true) {
+  //           navigate("/");
+  //         }else {
+  //           navigate("/");
+  //         }
+  //       })
+  //       .catch((err) => console.log(err));
+  //   };
+
+  return (
+    <div className="login-container">
+      <div className="form-container">
+        <Form /*onSubmit={handleSubmit}*/>
+          <Form.Group className="mb-3" controlId="formBasicEmail">
+            <Form.Label>Correo electrónico</Form.Label>
+            <br></br>
+            <input type="email" placeholder="Ingrese su email" /*{...email}*/></input>
+          </Form.Group>
+
+          <Form.Group className="mb-3" controlId="formBasicPassword">
+            <Form.Label>Contraseña</Form.Label>
+            <br></br>
+            <input type="password" placeholder="Ingrese su contraseña" /*{...password}*/></input>
+          </Form.Group>
+          <button className="button-login" type="submit">
+            Acceder
+          </button>
+        </Form>
+      </div>
+
+      <div className="img-container">
+        <img src={imgLogin} alt="img-login" />
+      </div>
+    </div>
+  );
+};
+
+export default Login;
