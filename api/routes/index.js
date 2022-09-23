7a114d81aed6b99a09471669390319dea32f47ad
MatiasNasif <matiasezequielnasif@gmail.com>
2022-09-22 22:31:10 -0300

    admin routes and model

--- api/routes/index.js
+++ api/routes/index.js
@@ -1,7 +1,9 @@
 const express = require("express");
 const router = express.Router();
 const residents = require("./residents");
+const users = require ("./users")
 
 router.use("/residents", residents);
+router.use("/admin", users)
 
 module.exports = router;
