b2f3adfc04e474517fa8b99ffe6edabbc61c0040
Fabian <fabianlopez1988@gmail.com>
2022-10-04 03:28:14 -0300

    Responsive Design Culture

--- client/src/components/Culture/Culture.js
+++ client/src/components/Culture/Culture.js
@@ -4,11 +4,11 @@ import axios from "axios";
 import "./Culture.css";
 
 const Culture = () => {
-  const [cultures, setCultures] = useState([]);
+  // const [cultures, setCultures] = useState([]);
 
-  useEffect(() => {
-    axios.get("/api/culture").then((res) => setCultures(res.data));
-  }, []);
+  // useEffect(() => {
+  //   axios.get("/api/culture").then((res) => setCultures(res.data));
+  // }, []);
 
   return (
     <div className="culture-container">
@@ -21,8 +21,12 @@ const Culture = () => {
           <div>
             <div className="grid1">
               <div className="box left">
-                <h1>{cultures[0]?.title}</h1>
-                <p>{cultures[0]?.paragraph}</p>
+                <h1>Permiso</h1>
+                <h1 className="violet">Perdón</h1>
+                <h1>Gracias</h1>
+                <p>Nuestro idioma debe estar presente en todo momento y en cada rincón de nuestro hogar. </p>
+                {/* <h1>{cultures[0]?.title}</h1>
+                <p>{cultures[0]?.paragraph}</p> */}
               </div>
               <div className="container-lines">
                 <div className="first-line"></div>
@@ -30,24 +34,36 @@ const Culture = () => {
               </div>
 
               <div className="box right">
-                <h1>{cultures[1]?.title}</h1>
-                <p>{cultures[1]?.paragraph}</p>
+                <h1>NO FOTOS</h1>
+                <h1 className="violet">NO</h1>
+                <h1>videos</h1>
+                <p>Cuidar el clima de conexión musical es parte de nuestra misión. Es por esta misma razón que no permitimos capturar momentos con aparatos tecnológicos. </p>
+                {/* <h1>{cultures[2]?.title}</h1>
+                <p>{cultures[2]?.paragraph}</p> */}
               </div>
             </div>
 
             <div className="grid2">
               <div className="box center-left">
-                <h1>{cultures[2]?.title}</h1>
-                <p>{cultures[2]?.paragraph}</p>
+                <h1>NO VIP</h1>
+                <h1>SOMOS TODOS</h1>
+                <h1 className="violet">iguales</h1>
+                <p>Es importante comprender que en nuestros encuentros somos todos iguales. No contamos con sectores exclusivos.</p>
+                {/* <h1>{cultures[3]?.title}</h1>
+                <p>{cultures[3]?.paragraph}</p> */}
               </div>
               <div className="second-line"></div>
             </div>
 
-            {/* <div className="box center-right">
-              <h1>{cultures[3]?.title}</h1>
-              <p>{cultures[3]?.paragraph}</p>
-            </div> */}
-            {/* <div className="third-line"></div> */}
+            <div className="grid3">
+              <div className="third-line"></div>
+              <div className="box center-right">
+                <h1><span className="violet">Green </span>club</h1>
+                <p>Somos conscientes del impacto que generamos en el medio ambiente. Hoy podemos decir que reciclamos la totalidad de los envases que utilizamos en cada uno de nuestros eventos (plástico, latas, y vidrios). Continuamos trabajando en alternativas que reduzcan el impacto ambiental y generen mayor conciencia acerca de la huella que dejamos.</p>
+                {/* <h1>{cultures[1]?.title}</h1>
+                <p>{cultures[1]?.paragraph}</p> */}
+              </div>
+            </div>
           </div>
         }
       </section>
--- client/src/components/Culture/IntroCulture/IntroCulture.js
+++ client/src/components/Culture/IntroCulture/IntroCulture.js
@@ -2,17 +2,23 @@ import React, { useState, useEffect } from "react";
 import axios from "axios";
 
 const IntroCulture = () => {
-  const [introCulture, setIntroCulture] = useState([]);
+  // const [introCulture, setIntroCulture] = useState([]);
 
-  useEffect(() => {
-    axios.get("/api/introculture").then((res) => setIntroCulture(res.data));
-  }, []);
+  // useEffect(() => {
+  //   axios.get("/api/introculture").then((res) => setIntroCulture(res.data));
+  // }, []);
 
   return (
     <section className="intro-culture">
-      {!introCulture
+      <p>
+        Durante 10 años, nos esforzamos en apoyar, contribuir, y desarrollar la
+        cultura techno desde nuestro lugar. Abrirnos a un camino de aprendizaje,
+        nos llevó a comprender que hay ciertos valores que son esenciales para
+        el crecimiento y el cuidado de dicha cultura:<br></br>Amor, respeto, humildad.
+      </p>
+      {/* {!introCulture
         ? null
-        : introCulture.map((intro) => <div>{intro.paragraph}</div>)}
+        : introCulture.map((intro) => <div>{intro.paragraph}</div>)} */}
     </section>
   );
 };
