f3a058db988b507c0e94d389979f198f1af755ef
Fabian <fabianlopez1988@gmail.com>
2022-10-03 23:49:26 -0300

    culture css

--- client/src/components/Culture/Culture.js
+++ client/src/components/Culture/Culture.js
@@ -19,25 +19,35 @@ const Culture = () => {
       <section className="culture-section">
         {
           <div>
-            <div className="box left">
-              <h1>{cultures[0]?.title}</h1>
-              <p>{cultures[0]?.paragraph}</p>
+            <div className="grid1">
+              <div className="box left">
+                <h1>{cultures[0]?.title}</h1>
+                <p>{cultures[0]?.paragraph}</p>
+              </div>
+              <div className="container-lines">
+                <div className="first-line"></div>
+                <div className="first-line2"></div>
+              </div>
+
+              <div className="box right">
+                <h1>{cultures[1]?.title}</h1>
+                <p>{cultures[1]?.paragraph}</p>
+              </div>
             </div>
-            <div className="box right">
-              <h1>{cultures[1]?.title}</h1>
-              <p>{cultures[1]?.paragraph}</p>
+
+            <div className="grid2">
+              <div className="box center-left">
+                <h1>{cultures[2]?.title}</h1>
+                <p>{cultures[2]?.paragraph}</p>
+              </div>
+              <div className="second-line"></div>
             </div>
-            <div className="box center-left">
-              <h1>{cultures[2]?.title}</h1>
-              <p>{cultures[2]?.paragraph}</p>
-            </div>
-            <div className="box center-right">
+
+            {/* <div className="box center-right">
               <h1>{cultures[3]?.title}</h1>
               <p>{cultures[3]?.paragraph}</p>
-            </div>
-            <div className="first-line"></div>
-            <div className="second-line"></div>
-            <div className="third-line"></div>
+            </div> */}
+            {/* <div className="third-line"></div> */}
           </div>
         }
       </section>
