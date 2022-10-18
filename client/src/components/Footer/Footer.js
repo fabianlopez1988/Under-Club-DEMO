c1db8975914caa28329d79cccb11484c350e36bb
Fabian <fabianlopez1988@gmail.com>
2022-10-18 03:11:30 -0300

    footer-home

--- client/src/components/Footer/Footer.js
+++ client/src/components/Footer/Footer.js
@@ -14,8 +14,60 @@ const Footer = () => {
   return (
     <>
       <footer className="footer-container">
+        <div id="links">
+          <a
+            href="https://www.instagram.com/under.club.oficial/"
+            target="_blank"
+            rel="noreferrer"
+          >
+            <h1>INSTAGRAM</h1>
+          </a>
+          <a
+            href="https://www.facebook.com/1derclub"
+            target="_blank"
+            rel="noreferrer"
+          >
+            <h1>FACEBOOK</h1>
+          </a>
+          <a
+            href="https://soundcloud.com/under-club"
+            target="_blank"
+            rel="noreferrer"
+          >
+            <h1>SOUNDCLOUD</h1>
+          </a>
+          <a href="https://ra.co/clubs/98071" target="_blank" rel="noreferrer">
+            <h1>RESIDENT ADVISOR</h1>
+          </a>
+          <a
+            href="https://www.youtube.com/c/UnderClubOficial"
+            target="_blank"
+            rel="noreferrer"
+          >
+            <h1>YOU TUBE</h1>
+          </a>
+        </div>
+        <div id="location-footer">
+          <h1>AV. CNEL. NICETO VEGA 5699</h1>
+          <h1>1414, BUENOS AIRES</h1>
+          <h1>ARGENTINA</h1>
+        </div>
+        <div></div>
+        <div id="link-to-contact">
+          <a
+            href="/contact"
+            rel="noreferrer"
+          >
+            <h1>ESCRIBINOS</h1>
+          </a>
+        </div>
+
+        <div>
+          <div></div>
+          <div></div>
+        </div>
         {/* <img src={foto} alt="foto-footer" /> */}
-        <ul className="icons">
+        {/* <ul className="icons">
           <li>
             <a
               href="https://www.facebook.com/1derclub"
@@ -81,7 +133,7 @@ const Footer = () => {
               </span>
             </a>
           </li>
-        </ul>
+        </ul> */}
       </footer>
     </>
   );
--- client/src/components/ShortHistory/ShortHistory.js
+++ client/src/components/ShortHistory/ShortHistory.js
@@ -8,27 +8,6 @@ const ShortHistory = () => {
     <>
     
     <Zoom>
-      {/* <div className="historyContainer" id="history">
-      <div className="">
-
-      <div className="titulo3Container">
-        <h1 className="titulo3">UNDER CLUB</h1>
-      </div>
-      <div className="titulo4Container">
-        <h1 className="titulo4 grid">PARA TODO EL MUNDO</h1>
-      </div>
-      <div className="grid-line-short">
-
-      </div>
-      </div>
-
-      <p>
-        “Entonces ahí te das cuenta que hay algo más, que no es solo un Club de
-        música electrónica, que pasan cosas que impactan en la vida de las
-        personas”.
-        <br></br>Joel Silva.
-      </p>
-    </div> */}
 
       <div className="short-history-container">
         <div>
