84ffb4a54fb2d66caaf5a5b6a27f43f0e064fb04
fabian <fabianlopez1988@gmail.com>
2022-12-05 01:34:01 -0300

    compose

--- api/config/db.js
+++ api/config/db.js
@@ -1,32 +1,15 @@
 const Sequelize = require("sequelize");
+
 require('dotenv').config();
-const db = new Sequelize('UCWeb', null, null, {
-    host: 'localhost',
+
+const { DB_NAME, PSQL_USER, PSQL_PASS, DB_PORT, DB_HOST } = process.env;
+
+const db = new Sequelize(DB_NAME, PSQL_USER, PSQL_PASS, {
+    port: DB_PORT,
+    host: DB_HOST,
     dialect: 'postgres',
     logging: false,
 });
-
-// const devConfig = {
-//   database: process.env.PG_DATABASE,
-//   user: process.env.PG_USER,
-//   password: process.env.PG_PASSWORD,
-//   host: process.env.PG_HOST,
-//   dialect: "postgres",
-//   logging: false,
-// };
-
-// const db = new Sequelize(devConfig);
-
-// const db = new Sequelize(
-//   process.env.PG_DATABASE,
-//   process.env.PG_USER,
-//   process.env.PG_PASSWORD,
-//   {
-//     host: process.env.PG_HOST,
-//     dialect: process.env.PG_DIALECT,
-//     logging: false,
-//   }
-// );
 
 module.exports = db;
 
