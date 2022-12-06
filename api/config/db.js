e443dbb2b6dad0ff25d2b62f7009908e2d0c3c8c
Melisa <d.melisaburgos@gmail.com>
2022-12-06 01:19:19 -0300

    docker compose

--- api/config/db.js
+++ api/config/db.js
@@ -1,7 +1,9 @@
 const Sequelize = require("sequelize");
-// require('dotenv').config();
-const db = new Sequelize('UCWeb', null, null, {
-    host: 'localhost',
+require('dotenv').config();
+const { DB_NAME, PSQL_USER, PSQL_PASS, DB_PORT, DB_HOST } = process.env;
+const db = new Sequelize(DB_NAME, PSQL_USER, PSQL_PASS, {
+    port: DB_PORT,
+    host: DB_HOST || "localhost",
     dialect: 'postgres',
     logging: false,
 });
