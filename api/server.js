3383221953fe3bc141b13796d4564f9a27ab1b91
fabian <fabianlopez1988@gmail.com>
2022-12-23 01:29:08 -0300

    modified server dirname path4

--- api/server.js
+++ api/server.js
@@ -79,7 +79,7 @@ passport.deserializeUser(function (id, done) {
 //   res.sendFile('public', { root: __dirname })
 // });
 
-app.use(express.static(path.join(__dirname, "public")));
+// app.use(express.static(path.join(__dirname, "public")));
 
 app.get('/', function (req, res) {
   res.sendFile(__dirname + '/public/index.html');
